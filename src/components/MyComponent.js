71e792c0b601bd216e6140d933ca0580fa6addd9
JAVA_09_21110856 <21110856@student.hcmute.edu.vn>
2024-07-17 13:26:55 +0700

    state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,8 +1,12 @@
 import React from "react";
 class MyComponent extends React.Component{
+    state = {
+        name: "chuichien",
+        age: 21
+    };
     render(){
         return(
-            <div> My first Component </div>
+            <div> Tui ten la {this.state.name}. Nam nay tui {this.state.age} </div>
         );
     }
 }
