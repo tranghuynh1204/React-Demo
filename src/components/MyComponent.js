9c562f4de563ed4ed59518e66f16f8044f7799bc
JAVA_09_21110856 <21110856@student.hcmute.edu.vn>
2024-07-19 17:49:38 +0700

    set state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,9 +4,23 @@ class MyComponent extends React.Component{
         name: "chuichien",
         age: 21
     };
+    handleClick = (event) => {
+        console.log(">> click me my button")
+        this.setState({
+            name: 'Trang',
+            age: Math.floor((Math.random()*100) + 1)
+        })
+    }
+    handleOnMouseOuver(event){
+        console.log(event.pageX)
+
+    }
     render(){
         return(
-            <div> Tui ten la {this.state.name}. Nam nay tui {this.state.age} </div>
+            <div> Tui ten la {this.state.name}. Nam nay tui {this.state.age} 
+            <button onMouseOver={this.handleOnMouseOuver}>Hover me</button>
+            <button onClick={this.handleClick}>Click me</button>
+            </div>
         );
     }
 }
